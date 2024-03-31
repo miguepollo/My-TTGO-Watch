4ebf1a80b837146fef5639535db41eafb7747b04
miguepollo <miguelgarciapel@protonmail.com>
2024-03-31 10:15:36 +0200

    Change debug level

--- src/hardware/hardware.cpp
+++ src/hardware/hardware.cpp
@@ -175,6 +175,7 @@ void hardware_setup( void ) {
              * lvgl helper
              */
             lvgl_7helper.lvgl_begin();
+            lvgl_7helper.setupTouch();
         #elif defined( LILYGO_WATCH_2021 )
             /**
              * power all devices
